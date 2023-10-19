d90ef8eaccf60bae584e595cfafa7de19f64c18a
justplayeryt <68595906+justplayeryt@users.noreply.github.com>
2023-10-19 11:37:36 +0200

    script.js

--- script.js
+++ script.js
@@ -23,23 +23,23 @@ document.body.addEventListener("keydown", snakeMove)
 
 function snakeMove(event) {
     isGameStarted = true
-    if(event.keyCode==87){
+    if(event.keyCode==38){
         if(movementStatus!="down") {
             movementStatus = "up"
         } 
     }
-    if(event.keyCode==83){
+    if(event.keyCode==40){
         if(movementStatus!="up") {
             movementStatus = "down"
         }
     }
-    if(event.keyCode==68){
+    if(event.keyCode==39){
         if(movementStatus!="left") {
             movementStatus = "right"
         }
         
     }
-    if(event.keyCode==65){
+    if(event.keyCode==37){
         if(movementStatus!="right") {
             movementStatus = "left"
         }
